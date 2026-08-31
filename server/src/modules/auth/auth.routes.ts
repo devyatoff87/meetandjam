ce7c8dc18bafe8712d1f2e958a60dee4fe1a5d51
Vladimir Devyatov <v.devyatov@gmail.com>
2026-08-31 21:29:57 +0200

    feat(auth): implement user registration endpoint

---
+++ server/src/modules/auth/auth.routes.ts
@@ -0,0 +1,49 @@
+import { FastifyPluginAsync } from "fastify";
+import { AppDataSource } from "../../db/data-source";
+import { User } from "../../db/entities/user.entity";
+import { registerSchema } from "./auth.schemas";
+import argon2 from "argon2";
+
+export const authRoutes: FastifyPluginAsync = async (app) => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  app.post("register", async (request, reply) => {
+    const parseBody = registerSchema.safeParse(request.body);
+
+    if (!parseBody.success) {
+      return reply.code(400).send({
+        message: "Validation error",
+        error: parseBody.error,
+      });
+    }
+
+    const { name, email, password } = parseBody.data;
+    const isUserExist = await userRepository.find({ where: { email } });
+    if (isUserExist) {
+      return reply
+        .code(409)
+        .send({ message: "User with this email already exists" });
+    }
+    const passwordHash = await argon2.hash(password);
+    const user = userRepository.create({
+      email,
+      passwordHash,
+      name,
+    });
+
+    const savedUser = await userRepository.save(user);
+    const token = await reply.jwtSign({
+      sub: savedUser.id,
+      email: savedUser.email,
+    });
+
+    reply.code(201).send({
+      token,
+      user: {
+        sub: savedUser.id,
+        email: savedUser.email,
+        name: savedUser.name,
+      },
+    });
+  });
+};
