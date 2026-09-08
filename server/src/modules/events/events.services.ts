d2b9d40b81899e64de343b738643c1525c1d120d
Vladimir Devyatov <v.devyatov@gmail.com>
2026-09-08 15:56:52 +0200

    feat(Event): add services for joining an event

--- server/src/modules/events/events.services.ts
+++ server/src/modules/events/events.services.ts
@@ -1,9 +1,11 @@
 import { AppDataSource } from "../../db/data-source";
 import { Event } from "../../db/entities/event.entity";
-import { checkAdminship, checkEventOwnership } from "../helpers";
+import { EventParticipant } from "../../db/entities/participant.entity";
+import { checkAdminship, checkEventOwnership, errorTrigger } from "../helpers";
 
 export class EventsService {
   private eventRepository = AppDataSource.getRepository(Event);
+  private participantRepository = AppDataSource.getRepository(EventParticipant);
 
   async create(data: any, ownerId: string) {
     const event = this.eventRepository.create({
@@ -21,23 +23,27 @@ export class EventsService {
 
   async findOne(id: string, userId: string) {
     const { event, isOwner, isAdmin } = await checkEventOwnership(id, userId);
-    if (!event) {
-      throw new Error("Event not found");
-    }
-    if (!isOwner && !isAdmin) {
-      throw new Error("Forbidden");
-    }
+
+    errorTrigger(!event, "Event not found", 404);
+    errorTrigger(
+      !isOwner && !isAdmin,
+      "You don't have access for the action",
+      403,
+    );
+
     return event;
   }
 
   async update(id: string, userId: string, data: any) {
     const { event, isOwner, isAdmin } = await checkEventOwnership(id, userId);
-    if (!event) {
-      throw new Error("Event not found");
-    }
-    if (!isOwner && !isAdmin) {
-      throw new Error("Forbidden");
-    }
+
+    errorTrigger(!event, "Event not found", 404);
+    errorTrigger(
+      !isOwner && !isAdmin,
+      "You don't have access for the action",
+      403,
+    );
+
     return await this.eventRepository.save({
       ...event,
       ...data,
@@ -46,24 +52,45 @@ export class EventsService {
 
   async delete(id: string, userId: string) {
     const { event, isOwner, isAdmin } = await checkEventOwnership(id, userId);
-    if (!event) {
-      throw new Error("Event not found");
-    }
-    if (!isOwner && !isAdmin) {
-      throw new Error("Forbidden");
-    }
-    await this.eventRepository.remove(event);
+
+    errorTrigger(
+      !isOwner && !isAdmin,
+      "You don't have access to the action.",
+      403,
+    );
+
+    errorTrigger(!event, "Event not found", 404);
+
+    event && (await this.eventRepository.remove(event));
   }
 
   async deleteAll(userId: string) {
     const isAdmin = await checkAdminship(userId);
-    if (!isAdmin) {
-      throw new Error("Admin access required");
-    }
+
+    errorTrigger(!isAdmin, "Admin access required", 403);
+
     await this.eventRepository
       .createQueryBuilder()
       .delete()
       .from(Event)
       .execute();
+  }
+
+  async joinEvent(eventId: string, userId: string): Promise<EventParticipant> {
+    const event = await this.eventRepository.findOne({
+      where: { id: eventId },
+    });
+
+    errorTrigger(!event, "Event not found", 404);
+
+    const joinedToEvent = await this.participantRepository.findOne({
+      where: { eventId, userId },
+    });
+    errorTrigger(!joinedToEvent, "You have already joined this event", 409);
+
+    return await this.participantRepository.save({
+      eventId,
+      userId,
+    });
   }
 }
