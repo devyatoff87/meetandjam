c9b14cc91e5ddcb38fddb27458764388c9976c25
Vladimir Devyatov <v.devyatov@gmail.com>
2026-09-08 15:03:55 +0200

    refactor(Auth): adjust the auth router proper to new auth service

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,14 +1,12 @@
 import { FastifyPluginAsync } from "fastify";
-import { AppDataSource } from "../../db/data-source";
-import { User } from "../../db/entities/user.entity";
 import { loginSchema, registerSchema } from "./auth.schemas";
-import argon2 from "argon2";
 import { sendError } from "../helpers";
+import AuthService from "./auth.service";
 
 export const authRoutes: FastifyPluginAsync = async (app) => {
-  const userRepository = AppDataSource.getRepository(User);
+  const authService = new AuthService();
 
-  //REGISTER
+  // REGISTER
   app.post("/register", async (request, reply) => {
     const parseBody = registerSchema.safeParse(request.body);
 
@@ -16,33 +14,30 @@ export const authRoutes: FastifyPluginAsync = async (app) => {
       return sendError(reply, 400, "Validation error", parseBody.error);
     }
 
-    const { name, email, password } = parseBody.data;
-
-    const existingUser = await userRepository.findOne({ where: { email } });
-    if (existingUser) {
-      return sendError(reply, 409, "User with this email already exists");
-    }
-
-    const passwordHash = await argon2.hash(password);
-    const user = userRepository.create({ email, passwordHash, name });
-    const savedUser = await userRepository.save(user);
-
-    const token = await reply.jwtSign({
-      sub: savedUser.id,
-      email: savedUser.email,
-    });
-
-    return reply.code(201).send({
-      token,
-      user: {
-        id: savedUser.id,
+    try {
+      const savedUser = await authService.register(parseBody.data);
+      const token = await reply.jwtSign({
+        sub: savedUser.id,
         email: savedUser.email,
-        name: savedUser.name,
-      },
-    });
+      });
+
+      return reply.code(201).send({
+        token,
+        user: {
+          id: savedUser.id,
+          email: savedUser.email,
+          name: savedUser.name,
+        },
+      });
+    } catch (error: any) {
+      if (error.message === "An account with this email is already exists") {
+        return sendError(reply, 409, error.message);
+      }
+      return sendError(reply, 400, error.message);
+    }
   });
 
-  //LOGIN
+  // LOGIN
   app.post("/login", async (request, reply) => {
     const parseBody = loginSchema.safeParse(request.body);
 
@@ -50,51 +45,41 @@ export const authRoutes: FastifyPluginAsync = async (app) => {
       return sendError(reply, 400, "Validation error", parseBody.error);
     }
 
-    const { email, password } = parseBody.data;
+    try {
+      const user = await authService.login(parseBody.data);
+      const token = await reply.jwtSign({
+        sub: user.id,
+        email: user.email,
+      });
 
-    const user = await userRepository.findOne({ where: { email } });
-    if (!user) {
-      return sendError(reply, 401, "Login or password is not correct");
+      return reply.send({
+        token,
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        },
+      });
+    } catch (error: any) {
+      return sendError(reply, 401, error.message);
     }
+  });
 
-    const isPasswordValid = await argon2.verify(user.passwordHash, password);
-    if (!isPasswordValid) {
-      return sendError(reply, 401, "Login or password is not correct");
-    }
+  // GET /me
+  app.get("/me", { preHandler: [app.authenticate] }, async (request, reply) => {
+    const userId = request.user.sub;
 
-    const token = await reply.jwtSign({
-      sub: user.id,
-      email: user.email,
-    });
-
-    return reply.send({
-      token,
-      user: {
+    try {
+      const user = await authService.me(userId);
+      return reply.code(200).send({
         id: user.id,
         email: user.email,
         name: user.name,
-      },
-    });
-  });
-
-  //GET AUTHORIZED USERS DATA
-  app.get("/me", { preHandler: [app.authenticate] }, async (request, reply) => {
-    const userId = request.user.sub;
-    const user = await userRepository.findOne({
-      where: { id: userId },
-      select: ["id", "email", "name", "createdAt", "updatedAt"],
-    });
-
-    if (!user) {
-      return sendError(reply, 404, "User could not be found");
+        updatedAt: user.updatedAt,
+        createdAt: user.createdAt,
+      });
+    } catch (error: any) {
+      return sendError(reply, 404, error.message);
     }
-
-    return reply.code(200).send({
-      id: user.id,
-      email: user.email,
-      name: user.name,
-      updatedAt: user.updatedAt,
-      createdAt: user.createdAt,
-    });
   });
 };
