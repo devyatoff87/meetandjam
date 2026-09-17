85c924b3792f0575bcb9abdbd02c0b464b2b6d1c
Vladimir Devyatov <v.devyatov@gmail.com>
2026-09-17 16:41:49 +0200

    feat(setup-front): add global query provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryProvider } from "./query.provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,8 +20,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="flex min-h-full flex-col">{children}</body>
-    </html>
+    <QueryProvider>
+      <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+        <body className="flex min-h-full flex-col">{children}</body>
+      </html>
+    </QueryProvider>
   );
 }
