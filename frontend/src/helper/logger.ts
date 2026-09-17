0d2a46dcac581487fd7cff89caba9c34bdec8cd0
Vladimir Devyatov <v.devyatov@gmail.com>
2026-09-17 19:02:14 +0200

    fix(helper-frontend): type the 2nd array argument of logger to unkown

--- frontend/src/helper/logger.ts
+++ frontend/src/helper/logger.ts
@@ -1,4 +1,4 @@
-export const log = (info: [string, any]) => {
+export const log = (info: [string, unknown]) => {
   console.log(info[0].toUpperCase() + ": ");
   console.log(info[1]);
   console.log("-------------------------------------------------");
